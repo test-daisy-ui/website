ae7afcc950c343c514a36ce5e43784e690190cf5
hanzlamateen <hanzlamateen@live.com>
2024-07-09 15:21:08 +0500

    Setup prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
-import React from "react";
-import "./App.css";
+import React from 'react'
+import './App.css'
 
 const App = () => {
   return (
     <div className="App">
       <button className="btn">Hello daisyUI</button>
     </div>
-  );
-};
+  )
+}
 
-export default App;
+export default App
