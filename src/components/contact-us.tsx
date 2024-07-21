e19dec4e765210e43b96081f810d69085e62ffec
hanzlamateen <hanzlamateen@live.com>
2024-07-21 16:19:38 +0500

    Added validation in contact form

--- src/components/contact-us.tsx
+++ src/components/contact-us.tsx
@@ -4,20 +4,51 @@ import emailConfig from '../constants/emailjs.json'
 import routes from '../constants/routes.json'
 import { usePathScrollToView } from '../hooks/usePathScrollToView'
 
+enum Status {
+  None,
+  Submitting,
+  Error
+}
+
 const ContactUs = () => {
   const myRef = usePathScrollToView(routes.ContactUs)
 
-  const [isSubmitting, setIsSubmitting] = useState(false)
-  const [stateMessage, setStateMessage] = useState('')
-  const [firstName, setFirstName] = useState('')
-  const [lastName, setLastName] = useState('')
-  const [email, setEmail] = useState('')
-  const [phone, setPhone] = useState('')
-  const [message, setMessage] = useState('')
+  const [state, setState] = useState({
+    status: Status.None,
+    statusMessage: '',
+    firstName: '',
+    lastName: '',
+    email: '',
+    phone: '',
+    message: ''
+  })
+  const { status, statusMessage, firstName, lastName, email, phone, message } = state
 
   const handleSendEmail = async () => {
     try {
-      setIsSubmitting(true)
+      setState({ ...state, status: Status.Submitting })
+
+      const isValidEmail = String(email)
+        .toLowerCase()
+        .match(
+          /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+        )
+      if (!isValidEmail) {
+        setState({ ...state, status: Status.Error, statusMessage: 'Please enter a valid email.' })
+        return
+      } else if (firstName.length < 3) {
+        setState({ ...state, status: Status.Error, statusMessage: 'First Name should be atleast 3 characters.' })
+        return
+      } else if (lastName.length < 3) {
+        setState({ ...state, status: Status.Error, statusMessage: 'Last Name should be atleast 3 characters.' })
+        return
+      } else if (phone.length < 7) {
+        setState({ ...state, status: Status.Error, statusMessage: 'Phone should be atleast 7 characters.' })
+        return
+      } else if (message.length < 10) {
+        setState({ ...state, status: Status.Error, statusMessage: 'Message should be atleast 10 characters.' })
+        return
+      }
 
       await emailjs.send(
         emailConfig.serviceId,
@@ -28,22 +59,21 @@ const ContactUs = () => {
         }
       )
 
-      setStateMessage('Message sent!')
+      setState({ ...state, status: Status.None, statusMessage: 'Message sent!' })
       console.log('SUCCESS!')
+
+      setTimeout(() => {
+        setState({ ...state, firstName: '', lastName: '', email: '', phone: '', message: '', statusMessage: '' })
+      }, 5000) // hide message after 5 seconds
     } catch (err) {
       if (err instanceof EmailJSResponseStatus) {
-        setStateMessage('Something went wrong, please try again later')
         console.log('EMAILJS FAILED...', err)
-        return
       }
+
+      setState({ ...state, status: Status.Error, statusMessage: 'Something went wrong, please try again later' })
 
       console.log('ERROR', err)
     }
-
-    setTimeout(() => {
-      setStateMessage('')
-    }, 5000) // hide message after 5 seconds
-    setIsSubmitting(false)
   }
 
   return (
@@ -56,40 +86,49 @@ const ContactUs = () => {
         <div className="mt-10 grid grid-cols-2 gap-4">
           <input
             type="text"
+            disabled={status === Status.Submitting}
             placeholder="First Name"
             className="input w-full bg-[#E4E4E4]"
-            onChange={(e) => setFirstName(e.target.value)}
+            onChange={(e) => setState({ ...state, status: Status.None, statusMessage: '', firstName: e.target.value })}
           />
           <input
             type="text"
+            disabled={status === Status.Submitting}
             placeholder="Last Name"
             className="input w-full bg-[#E4E4E4]"
-            onChange={(e) => setLastName(e.target.value)}
+            onChange={(e) => setState({ ...state, status: Status.None, statusMessage: '', lastName: e.target.value })}
           />
           <input
             type="text"
+            disabled={status === Status.Submitting}
             placeholder="Email"
             className="input w-full bg-[#E4E4E4]"
-            onChange={(e) => setEmail(e.target.value)}
+            onChange={(e) => setState({ ...state, status: Status.None, statusMessage: '', email: e.target.value })}
           />
           <input
             type="text"
+            disabled={status === Status.Submitting}
             placeholder="Phone Number"
             className="input w-full bg-[#E4E4E4]"
-            onChange={(e) => setPhone(e.target.value)}
+            onChange={(e) => setState({ ...state, status: Status.None, statusMessage: '', phone: e.target.value })}
           />
         </div>
 
         <textarea
-          className="textarea textarea-lg bg-[#E4E4E4]"
+          disabled={status === Status.Submitting}
           placeholder="Message"
-          onChange={(e) => setMessage(e.target.value)}
+          className="textarea textarea-lg bg-[#E4E4E4]"
+          onChange={(e) => setState({ ...state, status: Status.None, statusMessage: '', message: e.target.value })}
         />
 
-        {isSubmitting && <p>Sending...</p>}
-        {stateMessage && <p>{stateMessage}</p>}
+        {status === Status.Submitting && <p>Sending...</p>}
+        {statusMessage && <p style={{ color: status === Status.Error ? 'orangered' : 'limegreen' }}>{statusMessage}</p>}
 
-        <button className="btn btn-primary mx-auto mt-6" onClick={handleSendEmail}>
+        <button
+          className="btn btn-primary mx-auto mt-6"
+          disabled={status === Status.Submitting}
+          onClick={handleSendEmail}
+        >
           Send
         </button>
       </div>
